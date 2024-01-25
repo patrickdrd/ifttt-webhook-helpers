1cad15064308b111487b718c3c2c69cad72894b7
patrickdrd <24896778+patrickdrd@users.noreply.github.com>
2024-01-25 09:55:27 +0200

    Update resolve-tco-links.ts

--- api/resolve-tco-links.ts
+++ api/resolve-tco-links.ts
@@ -2,25 +2,31 @@ import type {VercelApiHandler} from '@vercel/node'
 import {request} from 'undici'
 
 const handler: VercelApiHandler = async (req, res) => {
-	let {text} = req.body as {text: string}
-	const occurences = text.matchAll(/https:\/\/t\.co\/\w{10}/g)
+    let {text = ''} = req.body as {text: string}
+    const domains = ["t.co", "bit.ly", "dlvr.it"]
+//  let {text = '', domains = []} = req.body as {text: string; domains: string[]}
+    const domainsGroup = domains.map(d => d.replaceAll('.', '\\.')).join('|')
+    const occurences = text.matchAll(new RegExp(`https?://(?:${domainsGroup})/\\w+`, 'g'))
 
-	const toReplace = new Map<string, string>()
-	for (const [link] of occurences) {
-		if (toReplace.has(link)) {
-			continue
-		}
+//  let {text} = req.body as {text: string}
+//  const occurences = text.matchAll(/https:\/\/t\.co\/\w{10}/g)
 
-		// eslint-disable-next-line no-await-in-loop
-		const {headers: {location}} = await request(link, {method: 'HEAD'})
-		toReplace.set(link, typeof location === 'string' ? location : '')
-	}
+    const toReplace = new Map<string, string>()
+    for (const [link] of occurences) {
+        if (toReplace.has(link)) {
+            continue
+        }
 
-	for (const [link, resolvedUrl] of toReplace) {
-		text = text.replaceAll(link, resolvedUrl)
-	}
+        // eslint-disable-next-line no-await-in-loop
+        const {headers: {location}} = await request(link, {method: 'HEAD'})
+        toReplace.set(link, typeof location === 'string' ? location : '')
+    }
 
-	res.send({text})
+    for (const [link, resolvedUrl] of toReplace) {
+        text = text.replaceAll(link, resolvedUrl)
+    }
+
+    res.send({text})
 }
 
 export default handler
